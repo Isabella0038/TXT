cb8e3630a84b1b47b2da1163240ab6fd23f5dc71
Isabella0038 <142461650+Isabella0038@users.noreply.github.com>
2023-10-17 18:43:10 -0500

    Add files via upload

---
+++ TXT/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace TXT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(159, 107);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(126, 61);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "ABRIR";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(358, 107);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(126, 61);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "GUARDAR";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(546, 107);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(126, 61);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "EDITAR";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(737, 107);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(126, 61);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "BORRAR";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(78, 205);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(861, 251);
+            this.txtContent.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ArchivoTXT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 527);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TXT/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TXT
+{
+    public partial class Form1 : Form
+    {
+        private string txtFilePath; // Ruta del archivo TXT
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            // Mostrar el cuadro de diálogo de apertura de archivo
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos de texto|*.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath = openFileDialog.FileName;
+                LoadAndDisplayTxtData();
+                MessageBox.Show("Archivo de texto cargado exitosamente.");
+            }
+        }
+
+        private void LoadAndDisplayTxtData()
+        {
+            try
+            {
+                // Leer todos los datos del archivo de texto y mostrarlos en un cuadro de texto
+                using (StreamReader reader = new StreamReader(txtFilePath))
+                {
+                    string content = reader.ReadToEnd();
+                    txtContent.Text = content;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar y mostrar desde el archivo de texto: " + ex.Message);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Guardar el contenido del TextBox txtContent de vuelta al archivo de texto
+                using (StreamWriter writer = new StreamWriter(txtFilePath))
+                {
+                    writer.Write(txtContent.Text);
+                }
+
+                MessageBox.Show("Guardado en el archivo de texto.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar en el archivo de texto: " + ex.Message);
+            }
+        }
+
+        // Evento para el botón "Editar"
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            // Verificar si hay algo seleccionado en el TextBox
+            if (!string.IsNullOrWhiteSpace(txtContent.SelectedText))
+            {
+                // Obtener el texto seleccionado
+                string selectedText = txtContent.SelectedText;
+
+                // Mostrar un cuadro de diálogo para editar el texto seleccionado
+                string editedText = Microsoft.VisualBasic.Interaction.InputBox("Editar texto:", "Edición", selectedText);
+
+                // Reemplazar el texto seleccionado con el texto editado
+                txtContent.Text = txtContent.Text.Replace(selectedText, editedText);
+            }
+            else
+            {
+                MessageBox.Show("Seleccione el texto que desea editar.");
+            }
+        }
+
+        // Evento para el botón "Borrar"
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Verificar si hay algo seleccionado en el TextBox
+            if (!string.IsNullOrWhiteSpace(txtContent.SelectedText))
+            {
+                // Borrar el texto seleccionado
+                txtContent.Text = txtContent.Text.Replace(txtContent.SelectedText, "");
+            }
+            else
+            {
+                MessageBox.Show("Seleccione el texto que desea borrar.");
+            }
+        }
+    }
+}
